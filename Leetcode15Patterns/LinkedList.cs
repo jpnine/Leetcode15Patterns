bca887a7107fd3e52e6191c0c41572f41465a61a
Jaimin <jaimin.ece@gmail.com>
2025-11-19 23:35:51 -0500

    Add LinkedList class with ListNode and GetLength

---
+++ Leetcode15Patterns/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Leetcode15Patterns
+{
+    public class LinkedList
+    {
+        public class ListNode(int val = 0, ListNode? next = null)
+        {
+            public int val = val;
+            public ListNode next = next;
+        }
+
+        public int GetLength(ListNode head)
+        {
+            int length = 0;
+            ListNode current = head;
+            while (current != null)
+            {
+                length++;
+                current = current.next;
+            }
+            return length;
+        }
+    }
+}
