b323c28fa5c9225bc4289299c3860c0256305537
Jaimin <jaimin.ece@gmail.com>
2025-12-07 23:40:30 -0500

    Unit Test PreFixSum - Multiple Case into Theory

--- Leetcode15Patterns.Tests/PrefixSumTest.cs
+++ Leetcode15Patterns.Tests/PrefixSumTest.cs
@@ -1,39 +1,15 @@
-﻿
-using System.Diagnostics.Contracts;
-
-namespace Leetcode15Patterns.Tests
+﻿namespace Leetcode15Patterns.Tests
 {
     public class PrefixSumTest
     {
-        [Fact]
-        public void CalculatePrefixSum_ReturnsCorrectSum()
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 1, 3, 9)]
+        [InlineData(new int[] { 10, 20, 30, 40, 50 }, 2, 2, 30)]
+        [InlineData(new int[] { 5, 10, 15 }, 0, 2, 30)]
+        public void CalculatePrefixSum_ReturnsCorrectSum(int[] nums, int a, int b, int expectedSum)
         {
-            int[] nums = [1, 2, 3, 4, 5];
-            int a = 1;
-            int b = 3;
-            int result = PrefixSum.CalculatePrefixSum(nums, a, b);
-            Assert.Equal(9, result); // 2 + 3 + 4 = 9
+            int actualSum = PrefixSum.CalculatePrefixSum(nums, a, b);
+            Assert.Equal(expectedSum, actualSum);
         }
-
-        [Fact]
-        public void CalculatePrefixSum_SingleElement_ReturnsElementValue()
-        {
-            int[] nums = [10, 20, 30, 40, 50];
-            int a = 2;
-            int b = 2;
-            int result = PrefixSum.CalculatePrefixSum(nums, a, b);
-            Assert.Equal(30, result); // Only the element at index 2
-        }
-
-        [Fact]
-        public void CalculatePrefixSum_FullArray_ReturnsTotalSum()
-        {
-            int[] nums = [5, 10, 15];
-            int a = 0;
-            int b = 2;
-            int result = PrefixSum.CalculatePrefixSum(nums, a, b);
-            Assert.Equal(30, result); // 5 + 10 + 15 = 30
-        }
-
     }
 }
