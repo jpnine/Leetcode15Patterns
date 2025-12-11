0f71d452eccfb2ab92a98d372491cb200ec78342
Jaimin <jaimin.ece@gmail.com>
2025-12-11 00:14:23 -0500

    Unit Test Sliding Window

---
+++ Leetcode15Patterns.Tests/SlidingWindowTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Leetcode15Patterns.Tests
+{
+    public class SlidingWindowTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 1, 2, 3 }, 4)]
+        public void TestFruitsIntoBasket(int[] fruits, int expected)
+        {
+            var output = SlidingWindow.FruitsIntoBasket(fruits);
+            Assert.Equal(expected, output);
+        }
+    }
+}
